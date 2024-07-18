0bad8f1f8131e74e4aae9e565e60de95a0620891
Zakawat <zsubzwary@gmail.com>
2024-07-18 18:09:51 +0500

    Added docs to the Protected Routes

--- src/util/ProtectedRoutes.js
+++ src/util/ProtectedRoutes.js
@@ -1,6 +1,11 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { isUserLoggedIn } from './helper';
 
+/**
+ * Function to handle protected routes based on user authentication status.
+ *
+ * @return {JSX.Element} Either the Outlet component if user is authenticated or the Navigate component to redirect to login.
+ */
 export const ProtectedRoutes = () => {
   const auth = isUserLoggedIn();
   
