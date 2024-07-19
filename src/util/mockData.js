6627b7c7a0edac9c0d4e81a075ff74c64fb8efbd
Zakawat <zsubzwary@gmail.com>
2024-07-19 12:26:09 +0500

    Create mockData.js

---
+++ src/util/mockData.js
@@ -0,0 +1,32 @@
+export const priorityOptions = [
+  { value: "high", label: "High" },
+  { value: "medium", label: "Medium" },
+  { value: "low", label: "Low" },
+];
+
+export const dcSwitchStatusOptions = [
+  { value: "on", label: "On" },
+  { value: "off", label: "Off" },
+];
+
+export const statusOptions = [
+  { value: "active", label: "Active" },
+  { value: "inactive", label: "Inactive" },
+];
+
+export const roleOptions = [
+  { value: "admin", label: "Admin" },
+  { value: "standard", label: "Standard User" },
+];
+
+export const userOptions = [
+  { value: "john", label: "John" },
+  { value: "jacob", label: "Jacob" },
+  { value: "jennifer", label: "Jennifer" },
+];
+
+export const userData = {
+  john: { email: "john@example.com", phone: "123-456-7890" },
+  jacob: { email: "jacob@example.com", phone: "987-654-3210" },
+  jennifer: { email: "jennifer@example.com", phone: "555-123-4567" },
+};
