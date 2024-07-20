f257478357ccb828ce39f73efe57dbc8b178b785
Zakawat <zsubzwary@gmail.com>
2024-07-20 12:24:33 +0500

    Implementing Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Login from "./pages/login";
-import Dashboard from './pages/dashboard';
+import Dashboard from "./pages/dashboard";
 // import Issues from './Issues';
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ProtectedRoutes } from "./util/ProtectedRoutes";
 import Issues from "./pages/issues";
-import { DefaultTheme } from "./themes/default";
-import {createTheme} from "@mui/material";
+import { LightTheme } from "./themes/Light";
+import { DarkTheme } from "./themes/Dark";
+import { createTheme } from "@mui/material";
 
-const theme = createTheme(DefaultTheme);
+export const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-    <Router>
-      <CssBaseline />
-      <Routes>
-          <Route element={<ProtectedRoutes/>}>
-            <Route path="/" element={<Dashboard/>} />
-            <Route path="/issues" element={<Issues />} />
-          </Route>
+  const [mode, setMode] = useState("light");
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
 
-          <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
-    </ThemeProvider>
+  const theme = React.useMemo(() => createTheme(mode === "light" ? LightTheme : DarkTheme), [mode]);
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <CssBaseline />
+          <Routes>
+            <Route element={<ProtectedRoutes />}>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/issues" element={<Issues />} />
+            </Route>
+
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
---
+++ src/themes/Dark.js
@@ -0,0 +1,54 @@
+export const DarkTheme = {
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#e7463f",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          background: "radial-gradient(84.88% 84.88% at 50% 15.12%, #E7463F 0%, #EF8439 100%)",
+          border: 0,
+          borderRadius: 30,
+          boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+          color: "white",
+          height: 48,
+          padding: "0 30px",
+          fontWeight: 700,
+        },
+      },
+    },
+    MuiButtonBase: {
+      styleOverrides: {
+        root: {
+          "&.Mui-selected": {
+            background: "radial-gradient(84.88% 84.88% at 50% 15.12%, #E7463F 0%, #EF8439 100%)",
+            color: "white",
+          },
+          "&.Mui-selected > .MuiListItemIcon-root": {
+            color: "white",
+          },
+          "& .MuiListItemIcon-root": {
+            color: "#E7463F",
+          },
+          "&:hover": {
+            background:
+              "radial-gradient(84.88% 84.88% at 50% 15.12%, rgba(231, 70, 63, 0.2) 0%, rgba(239, 132, 57, 0.2) 100%)",
+          },
+        },
+      },
+    },
+  },
+  shape: {
+    borderRadius: 4,
+  },
+  typography: {
+    fontSize: 16,
+    fontWeightRegular: 400,
+  },
+};
