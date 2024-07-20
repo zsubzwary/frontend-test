ff35b665f4ebf24bee6ab608c703a79d5392f405
Zakawat <zsubzwary@gmail.com>
2024-07-20 07:58:02 +0500

    Added DataTranformer helper function
    Transforms input data into a new format with 'value' and 'label' properties, primarily used in react-select

--- src/util/helper.js
+++ src/util/helper.js
@@ -4,19 +4,17 @@
  * @return {boolean} Returns true if the logout was successful, false otherwise.
  */
 export const logout = () => {
-    let isLogoutSuccessful = false;
-    try {
-        localStorage.removeItem("loggedIn");
-        sessionStorage.removeItem("loggedIn");
-        isLogoutSuccessful = true;
-    } catch (error) {
-        console.error(error);
-    }
-    finally {
-        return isLogoutSuccessful;
-    }
-}
-
+  let isLogoutSuccessful = false;
+  try {
+    localStorage.removeItem("loggedIn");
+    sessionStorage.removeItem("loggedIn");
+    isLogoutSuccessful = true;
+  } catch (error) {
+    console.error(error);
+  } finally {
+    return isLogoutSuccessful;
+  }
+};
 
 /**
  * Checks if the user is logged in by checking the "loggedIn" item in both localStorage and sessionStorage.
@@ -24,13 +22,22 @@ export const logout = () => {
  * @return {boolean} Returns true if the user is logged in, false otherwise.
  */
 export const isUserLoggedIn = () => {
-    let auth = false;
-    try {
-        auth = (localStorage.getItem("loggedIn") === "true" || sessionStorage.getItem("loggedIn") === "true");
-    } catch (error) {
-        console.error(error);
-    }
-    finally {
-        return auth;
-    }
-}
+  let auth = false;
+  try {
+    auth =
+      localStorage.getItem("loggedIn") === "true" || sessionStorage.getItem("loggedIn") === "true";
+  } catch (error) {
+    console.error(error);
+  } finally {
+    return auth;
+  }
+};
+
+export const dataTransformer = (data) => {
+  return data.map((item) => {
+    return {
+      value: item.id,
+      label: item.name,
+    };
+  });
+};
